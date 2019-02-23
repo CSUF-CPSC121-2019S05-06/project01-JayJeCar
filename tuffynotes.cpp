fe4326ac9ad5a670a69551ad97284675387acfda
JayJeCar <jizzellebc@csu.fullerton.edu>
2019-02-22 19:28:14 -0800

    Still Working

--- tuffynotes.cpp
+++ tuffynotes.cpp
@@ -1,33 +1,49 @@
+//This program is designed to create notes
 #include <iostream>
 #include <string> //Header file needed to use string objects
 
-int main() {
-
-std::string title;
-std::string note;
-char choice;
-
-//Display the menu to get the user's choice
-std::cout << "Welcome to TuffyNotes" << std::endl;
-std::cout << "[C] Create a Note" << std::endl;
-std::cout << "[E] Exit" << std::endl;
-std::cin >> choice;
-
-//Validate user input
-switch (choice)
+void add_note()
 {
-  case 'c':
-  case 'C': std::cout << "Please enter the note's title: " << std::endl;
+  std::string title;
+  std::string note;
+
+  std::cout << "Please enter the note's title: " << std::endl;
   std::cin >> title;
   std::cout << "Please enter the note: " << std::endl;
   std::cin >> note;
-  std::cout << "Note added!" << std::endl;
-  break;
-  case 'e':
-  case 'E': std::cout << "Thank you for using TuffyNotes!" << std::endl;
-  break;
-  default: std::cout << "Invalid choice. PLease try again." << std::endl;
 }
 
-return 0;
+int main()
+{
+
+  char choice;
+
+  //Display the menu to get the user's choice
+  std::cout << "Welcome to TuffyNotes" << std::endl;
+  std::cout << "[C] Create a Note" << std::endl;
+  std::cout << "[E] Exit" << std::endl;
+  std::cin >> choice;
+
+  if (choice=='C')
+  {
+    add_note();   //Call the function
+    std::cout << "Note added!" << std::endl;
+  }
+
+  /*else if (choice == 'Q')
+  {
+
+  }
+  */
+
+  else if (choice == 'E')
+  {
+    std::cout << "Thank you for using TuffyNotes!" << std::endl;
+  }
+  else
+  {
+    std::cout << "Invalid choice. PLease try again." << std::endl;
+  }
+
+  return 0;
 }
